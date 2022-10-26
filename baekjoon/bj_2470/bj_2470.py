2ed0db4f53ae93e7459397038188c54bd80ee9f7
Choi Jongyun <70887404+jongyuni@users.noreply.github.com>
2022-10-26 15:21:31 +0900

    bj_2470

---
+++ baekjoon/bj_2470/bj_2470.py
@@ -0,0 +1,26 @@
+import sys
+
+N = int(input())
+nums = list(map(int, sys.stdin.readline().split()))
+i, j = 0, len(nums) - 1
+level = 2000000000
+answer = []
+nums.sort()
+
+while i <= j:
+    cur = nums[i] + nums[j]
+    if i == j:
+        break
+    if abs(cur) <= level:
+        level = abs(cur)
+        answer = [nums[i], nums[j]]
+    if cur < 0:
+        i += 1
+    elif cur > 0:
+        j -= 1
+    else:
+        answer = [nums[i], nums[j]]
+        break
+
+for a in answer:
+    print(a, end=' ')
