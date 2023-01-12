190cb1a1a6edb0267f54a56f168d15213f8df291
Choi Jongyun <70887404+jongyuni@users.noreply.github.com>
2023-01-12 20:47:45 +0900

    2 x n 타일링

---
+++ programmers/연습문제/2 x n 타일링.py
@@ -0,0 +1,10 @@
+def solution(n):
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 3
+
+    for i in range(4, n + 1):
+        dp[i] = (dp[i-2] + dp[i-1]) % 1000000007
+
+    return dp[n]
